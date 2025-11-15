0680b89431105768276d5702a4c9170ac61d4aae
QuangNo1Pro <levanquangh7@gmail.com>
2025-11-15 16:13:27 +0700

    Sửa

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import 'reflect-metadata';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
